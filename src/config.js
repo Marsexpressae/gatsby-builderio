fe6f024351da7dd598a506043427f02d4802ab65
Marsexpressae <84410044+Marsexpressae@users.noreply.github.com>
2021-05-21 03:10:17 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: uncomment next line and enter your own public API key
-  builderAPIKey: process.env.BUILDER_API_KEY
+  builderAPIKey: process.env.a2c133c5da9d426c9200884eb8de4ebd
 };
